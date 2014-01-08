c291d0fc1667db3be3740c638d52fa63d4342b40
Corbin Hughes <hughes.corbin@gmail.com>
2014-01-08 02:21:17 -0600

    -

--- torrent.cpp
+++ torrent.cpp
@@ -13,12 +13,35 @@
 #define ASSERT_TRUE(cond) ASSERT_TRUE_MSG(cond, "")
 #define ASSERT_EQUAL(x, y) ASSERT_EQUAL_MSG(x, y, "")
 
-void test_assert(bool cond, const std::string& msg, const std::string& file, const std::string& func, int line)
+#define ASSERT_THROWS_HELPER(expr, msg, file, func, line) \
+	do {\
+		bool threw = false;\
+		try {\
+			expr;\
+		} catch (...) { threw = true; }\
+		test_assert(threw, msg, file, func, line);\
+	} while (0)
+#define ASSERT_THROWS_MSG(expr, msg) ASSERT_THROWS_HELPER(expr, msg, __FILE__, __func__, __LINE__)
+#define ASSERT_THROWS(expr) ASSERT_THROWS_MSG(expr, "")
+
+inline void test_assert(bool cond, const std::string& msg, const std::string& file, const std::string& func, int line)
 {
 	if (!cond) {
-		throw std::runtime_error("Failed test: " + msg + " in " 
-								+ file + " in " + func + " on line " + std::to_string(line));
+		if (msg.empty()) {
+			throw std::runtime_error("Failed test in "
+									+ file + " in " + func 
+									+ " on line " + std::to_string(line));
+		} else {
+			throw std::runtime_error("Failed test: " + msg + ". " 
+									+ file + " in " + func 
+									+ " on line " + std::to_string(line));
+		}
 	}
+}
+
+inline void throw_fail(const std::string& msg, const std::string& file, const std::string& func, int line)
+{
+	test_assert(false, msg, file, func, line);
 }
 
 void test_int()
@@ -31,6 +54,11 @@ void test_int()
 	ASSERT_EQUAL(bencode(0), "i0e");
 	ASSERT_EQUAL(bencode(1099511627776ll), "i1099511627776e");
 	ASSERT_EQUAL(bencode(-17), "i-17e");
+	
+	ASSERT_THROWS(bdecode("iabce"));
+	ASSERT_THROWS(bdecode("ie"));
+	ASSERT_THROWS(bdecode("i-0e"));
+	ASSERT_THROWS(bdecode("i-ae"));
 }
 
 void test_string()
@@ -38,6 +66,15 @@ void test_string()
 	ASSERT_EQUAL(bdecode("5:hello"), bencode_value("hello"));
 	ASSERT_EQUAL(bdecode("11:hello world"), bencode_value("hello world"));
 	ASSERT_EQUAL(bdecode("9:!!!333@@@"), bencode_value("!!!333@@@"));
+	
+	ASSERT_EQUAL(bencode("hello"), "5:hello");
+	ASSERT_EQUAL(bencode("hello world"), "11:hello world");
+	ASSERT_EQUAL(bencode("13"), "2:13");
+	ASSERT_EQUAL(bencode("-13"), "3:-13");
+	
+	ASSERT_THROWS(bdecode("50:!!!333@@@"));
+	ASSERT_THROWS(bdecode("1:!!!333@@@"));
+	ASSERT_THROWS(bdecode("-9:!!!333@@@"));
 }
 
 void test_list()
@@ -46,7 +83,7 @@ void test_list()
 	const std::string encoded = "l4:spam4:eggse";
 	auto list = std::vector<bencode_value>{bencode_value("spam"), bencode_value("eggs")};
 	bencode_value expected = bencode_value(list);
-	ASSERT_TRUE(bdecode(encoded) == expected);
+	ASSERT_EQUAL(bdecode(encoded), expected);
 }
 
 void test_dict()
